94e6c2432389412d190f6a2000682034e2fc47b8
Igor Dvorkin <idvorkin@gmail.com>
2019-11-30 09:02:45 -0800

    Add markdown middleware

--- ZachWebSite/Startup.cs
+++ ZachWebSite/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Westwind.AspNetCore.Markdown;
 
 namespace ZachWebSite
 {
@@ -31,21 +32,35 @@ namespace ZachWebSite
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-
             services.AddRazorPages();
+            services.AddMarkdown(config =>
+            {
+                // https://www.codemag.com/Article/1811071/Marking-up-the-Web-with-ASP.NET-Core-and-Markdown
+                config.AddMarkdownProcessingFolder("/stories/");
+            });
+            // have to let MVC know we have a controller
+            services.AddMvc(config =>
+           {
+               config.EnableEndpointRouting = false; // TBD: Figure out what this does.
+           }
+
+            ).AddApplicationPart(typeof(MarkdownPageProcessorMiddleware).Assembly);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseRouting();
             app.UseDeveloperExceptionPage();
             app.UseHttpsRedirection();
+            app.UseMarkdown();
             app.UseStaticFiles();
             app.UseCookiePolicy();
+            app.UseRouting();
+            app.UseMvc();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
